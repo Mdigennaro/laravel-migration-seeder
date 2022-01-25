eda35407f42c87c6b1f03888bab5083e461b6915
Mdigennaro <michele.digennaro719@gmail.com>
2022-01-25 15:49:27 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('content')
+
+<main>
+  <div class="container">
+    <h1>HOME</h1>
+  </div>
+</main>
+  
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header class="container">
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled">Disabled</a>
+    </li>
+  </ul>
+</header>
